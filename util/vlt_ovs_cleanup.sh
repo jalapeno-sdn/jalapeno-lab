e4a7c1509978a5f387f8bcce9b64d335efbefa7e
brmcdoug <3236927+brmcdoug@users.noreply.github.com>
2019-01-17 09:08:19 -0800

    Update vlt_ovs_cleanup.sh

--- util/vlt_ovs_cleanup.sh
+++ util/vlt_ovs_cleanup.sh
@@ -10,19 +10,40 @@ ovs-vsctl del-port vlt_br rtr00xr2
 ovs-vsctl del-port vlt_br rtr00xr3
 ovs-vsctl del-port vlt_br rtr00xr4
 ovs-vsctl del-port vlt_br rtr00xr5
+ovs-vsctl del-port vlt_br rtr00xr6
+ovs-vsctl del-port vlt_br rtr00xr7
 ovs-vsctl del-port vlt_br rtr01xr0
 ovs-vsctl del-port vlt_br rtr01xr1
 ovs-vsctl del-port vlt_br rtr01xr2
+ovs-vsctl del-port vlt_br rtr01xr3
+ovs-vsctl del-port vlt_br rtr01xr4
+ovs-vsctl del-port vlt_br rtr01xr5
+ovs-vsctl del-port vlt_br rtr01xr6
+ovs-vsctl del-port vlt_br rtr01xr7
 ovs-vsctl del-port vlt_br rtr02xr0
 ovs-vsctl del-port vlt_br rtr02xr1
 ovs-vsctl del-port vlt_br rtr02xr2
+ovs-vsctl del-port vlt_br rtr02xr3
+ovs-vsctl del-port vlt_br rtr02xr4
+ovs-vsctl del-port vlt_br rtr02xr5
+ovs-vsctl del-port vlt_br rtr02xr6
+ovs-vsctl del-port vlt_br rtr02xr7
 ovs-vsctl del-port vlt_br rtr03xr0
 ovs-vsctl del-port vlt_br rtr03xr1
 ovs-vsctl del-port vlt_br rtr03xr2
+ovs-vsctl del-port vlt_br rtr03xr3
+ovs-vsctl del-port vlt_br rtr03xr4
+ovs-vsctl del-port vlt_br rtr03xr5
+ovs-vsctl del-port vlt_br rtr03xr6
+ovs-vsctl del-port vlt_br rtr03xr7
 ovs-vsctl del-port vlt_br rtr03xr0
 ovs-vsctl del-port vlt_br rtr03xr1
 ovs-vsctl del-port vlt_br rtr03xr2
-ovs-vsctl del-port vlt_k8s_br rtr00xr0
+ovs-vsctl del-port vlt_br rtr03xr3
+ovs-vsctl del-port vlt_br rtr03xr4
+ovs-vsctl del-port vlt_br rtr03xr5
+ovs-vsctl del-port vlt_br rtr03xr6
+ovs-vsctl del-port vlt_br rtr03xr7
 ovs-vsctl del-br vlt_br
 ovs-vsctl del-br vlt_mgt_br
 ovs-vsctl del-br vlt_k8s_br
