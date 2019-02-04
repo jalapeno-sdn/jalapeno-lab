bd52c203b07432b0119360be8ac52eb8b343e4f7
brmcdoug <3236927+brmcdoug@users.noreply.github.com>
2019-02-04 15:52:30 -0800

    Update restart_vlt_client.sh

--- util/restart_vlt_client.sh
+++ util/restart_vlt_client.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
-lxc-stop -n vlt_client
-ovs-vsctl del-port vlt_br client-eth0
+lxc-stop -n vltclient00
+ovs-vsctl del-port vlt_br vltclient00-eth0
 lxc-start -n vlt_client
 brctl delif lxcbr0 client-eth0
-ovs-vsctl add-port vlt_br client-eth0 tag=102
+ovs-vsctl add-port vlt_br vltclient00-eth0 tag=102
 echo lxc-ls --active
 lxc-ls --active
