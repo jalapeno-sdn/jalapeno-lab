d01a5efabaec7b2ca64a44660c4dc0038ca7cf32
brmcdoug <3236927+brmcdoug@users.noreply.github.com>
2019-02-04 15:46:38 -0800

    Update bridge_setup.sh

--- util/bridge_setup.sh
+++ util/bridge_setup.sh
@@ -3,21 +3,17 @@
 # this script creates Voltron's OVS bridge instances
 
 ovs-vsctl add-br vlt_br
-ovs-vsctl add-br vlt_k8s_br
 ovs-vsctl add-br vlt_mgt_br
 ovs-vsctl add-br vlt_outside_br
 
-ovs-vsctl show | grep vlt_br
-ovs-vsctl show | grep vlt_os
-ovs-vsctl show | grep vlt_mgt_br
-
 ifconfig vlt_br up
-ifconfig vlt_k8s_br up
 ifconfig vlt_mgt_br up
 ifconfig vlt_outside_br up
 
 ip addr add 10.251.251.1/24 dev vlt_mgt_br
 ip addr add 10.0.254.1/24 dev vlt_outside_br
 
-ip route add 10.0.250.0/24 via 10.0.254.2 dev vlt_outside_br
-
+ip route add 10.0.250.0/24 via 10.0.254.71 dev vlt_outside_br
+ip route add 10.0.129.0/24 via 10.0.254.72 dev vlt_outside_br
+ip route add 10.0.130.0/24 via 10.0.254.71 dev vlt_outside_br
+ip route add 10.0.201.0/24 via 10.0.254.76 dev vlt_outside_br
