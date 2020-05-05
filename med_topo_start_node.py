e3e448b36ab027872f68483bc5c2c18b99e03957
Bruce McDougall (brmcdoug) <3236927+brmcdoug@users.noreply.github.com>
2020-05-05 12:06:39 -0700

    Update med_topo_start_node.py

--- med_topo_start_node.py
+++ med_topo_start_node.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# This script starts routers in the Voltron demo topology - see .png file.  Interface/vswitch values are hardcoded 
+# This script starts routers in the Jalapeno demo topology - see .png file.  Interface/vswitch values are hardcoded 
 # into this script based on the topology file, however, if the user wishes to modify the topology they may do so by
 # changing the ovs-vsctl command values below
 
@@ -14,53 +14,45 @@ import subprocess
 
 #################################################################################
 
-# r00
-#gi0/0/0/0 - vlt_br/tag100 to centos/voltron ens4
-#gi0/0/0/1 - vlt_br/tag101 to voltron_api_gw_vlan
-#gi0/0/0/2 - vlt_br/tag102 to voltron_client1 & 2_vlan
-#gi0/0/0/3 - vlt_br/tag103 to r1 gi0/0/0/0
-#gi0/0/0/4 - vtl_br/tag104 to r2 gi0/0/0/0
-#gi0/0/0/5 - vlt_br/tag105 to r11 gi0/0/0/5
-#gi0/0/0/6 - vlt_br/tag106 to r12 gi0/0/0/4
-#gi0/0/0/7 - vlt_br/tag111 to r06 gi0/0/0/0
-#gi0/0/0/8 - available
-#gi0/0/0/9 - vlt_outside_br for scapy
+# R00
+#g0/0/0/0 - jlpn_br/tag100 to client VM (if exists)
+#g0/0/0/1 - jlpn_br/tag101 to R01 g0/0/0/0
+#g0/0/0/2 - jlpn_br/tag102 to R02 g0/0/0/0
+#g0/0/0/3 - jlpn_br/tag103 to R03 gi0/0/0/0
+#g0/0/0/4 - jlpn_br/tag104 to R04 gi0/0/0/0
+
 if (sys.argv[1]) in ['r00']:
     subprocess.call(['python', 'util/qemu-xrv9k.py', 'r00.img', '00', 'r00'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_mgt_br', 'rtr00mgt1'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr00xr0', 'tag=100'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr00xr1', 'tag=101'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'jlpn_mgt_br', 'rtr00mgt1'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'jlpn_br', 'r00ge0', 'tag=100'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'jlpn_br', 'rtr00xr1', 'tag=101'])
     subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr00xr2', 'tag=102'])
     subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr00xr3', 'tag=103'])
     subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr00xr4', 'tag=104'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr00xr5', 'tag=105'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr00xr6', 'tag=106'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr00xr7', 'tag=111'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_outside_br', 'rtr00xr9'])
 
-# r01 
-#gi0/0/0/0 - vlt_br/tag103 to r0 gi0/0/0/3
-#gi0/0/0/1 - vlt_br/tag400 to r7 gi0/0
-#gi0/0/0/5 - vlt_br/tag112 to r5 RR gi0/0/0/0
-#gi0/0/0/9 - vlt_outside_br for scapy
+# R01 
+#g0/0/0/0 - jlpn_br/tag103 to R00 g0/0/0/1
+#g0/0/0/1 - jlpn_br/tag400 to R03 g0/0/0/3
+#g0/0/0/5 - jlpn_br/tag112 to R04 g0/0/0/3
 
 if (sys.argv[1]) in ['r01']:
     subprocess.call(['python', 'util/qemu-xrv9k.py', 'r01.img', '01', 'r01'])
     subprocess.call(['ovs-vsctl', 'add-port', 'vlt_mgt_br', 'rtr01mgt1'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr01xr0', 'tag=103'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr01xr1', 'tag=400'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr01xr5', 'tag=112'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_outside_br', 'rtr01xr9'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr01xr0', 'tag=101'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr01xr1', 'tag=105'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr01xr5', 'tag=106'])
 
-# r05 
-#gi0/0/0/0 - vlt_br/tag112 to r1 gi0/0/0/5
-#gi0/0/0/2 - vlt_br/tag401 to r7 gi0/0/0/3
+# R02 
+#g0/0/0/0 - jlpn_br/tag104 to R00 gi0/0/0/4
+#g0/0/0/2 - vlt_br/tag107 to R03 gi0/0/0/4
+#g0/0/0/2 - vlt_br/tag108 to R04 gi0/0/0/4
 
 if (sys.argv[1]) in ['r05']:
     subprocess.call(['python', 'util/qemu-xrv9k.py', 'r05.img', '05', 'r05'])
     subprocess.call(['ovs-vsctl', 'add-port', 'vlt_mgt_br', 'rtr05mgt1'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr05xr0', 'tag=112'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr05xr2', 'tag=401'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr05xr0', 'tag=104'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr05xr2', 'tag=107'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr05xr2', 'tag=108'])
 
 # r11
 #gi0/0/0/2 -  vlt_br/tag105 to r00
@@ -120,84 +112,6 @@ if (sys.argv[1]) in ['r07']:
     subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr07xr2', 'tag=404'])
     subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr07xr7', 'tag=405'])
 
-##############################
-# Amazon DMVPN
-
-# r50 - corp site a R50
-#g1 -  vlt_outside_br
-#g2 -  vlt_br/tag500 to r51 g2
-#g3 -  vlt_br/tag505 to r55 g2
-#g4 -  vlt_mgt_br
-if (sys.argv[1]) in ['r50']:
-    subprocess.call(['virsh', 'start', 'r50'])
-
-# r51 - small pop RR51
-#g1 -  vlt_outside_br
-#g2 -  vlt_br/tag500 to r50 g2
-#g3 -  vlt_br/tag501 to r52 g2
-#g4 -  vlt_mgt_br
-if (sys.argv[1]) in ['r51']:
-    subprocess.call(['virsh', 'start', 'r51'])
-
-# r52 - AWS PE R52
-#g1 -  vlt_outside_br
-#g2 -  vlt_br/tag501 to r51 g3
-#g3 -  vlt_br/tag502 to r53 g2
-#g4 -  vlt_mgt_br
-if (sys.argv[1]) in ['r52']:
-    subprocess.call(['virsh', 'start', 'r52'])
-
-# r53 - AWS PE53
-#g1 -  vlt_outside_br
-#g2 -  vlt_br/tag502 to r52 g3 
-#g3 -  vlt_br/tag503 to r56 g2
-#g4 -  vlt_mgt_br
-#g5 -  vlt_br/tag509 to r58 g5
-if (sys.argv[1]) in ['r53']:
-    subprocess.call(['virsh', 'start', 'r53'])
-
-# r56 - lg pop R56
-#g1 -  vlt_outside_br
-#g2 -  vlt_br/tag503 to r53 g3 
-#g3 -  vlt_br/tag504 to r54 g2
-#g4 -  vlt_mgt_br
-#g5 -  vlt_br/tag510 to r57 g5
-if (sys.argv[1]) in ['r56']:
-    subprocess.call(['virsh', 'start', 'r56'])
-
-# r54 - DC R54
-#g1 -  vlt_outside_br
-#g2 -  vlt_br/tag504 to r56 g3
-#g3 -  vlt_br/tag508 to r58 g3
-#g4 -  vlt_mgt_br
-if (sys.argv[1]) in ['r54']:
-    subprocess.call(['virsh', 'start', 'r54'])
-
-# r55 - AWS PE55
-#g1 -  vlt_outside_br
-#g2 -  vlt_br/tag505 to r50 g3
-#g3 -  vlt_br/tag506 to r57 g2
-#g4 -  vlt_mgt_br
-if (sys.argv[1]) in ['r55']:
-    subprocess.call(['virsh', 'start', 'r55'])
-
-# r57 - AWS PE57
-#g1 -  vlt_outside_br
-#g2 -  vlt_br/tag506 to r55 g3 
-#g3 -  vlt_br/tag507 to r58 g2
-#g4 -  vlt_mgt_br
-#g5 -  vlt_br/tag510 to r56 g5
-if (sys.argv[1]) in ['r57']:
-    subprocess.call(['virsh', 'start', 'r57'])
-
-# r58 - lg pop R58
-#g1 -  vlt_outside_br
-#g2 -  vlt_br/tag507 to r57 g3 
-#g3 -  vlt_br/tag508 to r54 g3
-#g4 -  vlt_mgt_br
-#g5 -  vlt_br/tag509 to r53 g5
-if (sys.argv[1]) in ['r58']:
-    subprocess.call(['virsh', 'start', 'r58'])
 
 ############################
 print "node started"
