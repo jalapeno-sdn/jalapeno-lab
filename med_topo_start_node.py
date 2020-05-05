dcf905a5983abe31296336d3900e5ef893d1b1a9
Bruce McDougall (brmcdoug) <3236927+brmcdoug@users.noreply.github.com>
2020-05-05 12:19:55 -0700

    Update med_topo_start_node.py

--- med_topo_start_node.py
+++ med_topo_start_node.py
@@ -54,64 +54,55 @@ if (sys.argv[1]) in ['r05']:
     subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr05xr2', 'tag=107'])
     subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr05xr2', 'tag=108'])
 
-# r11
-#gi0/0/0/2 -  vlt_br/tag105 to r00
-#gi0/0/0/3 -  vlt_br/tag402 to r12
-#gi0/0/0/5 -  vlt_br/tag403 to r03
-
-if (sys.argv[1]) in ['r11']:
-    subprocess.call(['python', 'util/qemu-xrv9k.py', 'r11.img', '11', 'r11'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_mgt_br', 'rtr11mgt1'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr11xr2', 'tag=105'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr11xr3', 'tag=402'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr11xr5', 'tag=403'])
-
-# r12
-#gi0/0/0/0 -  vlt_br/tag402 to r11
-#gi0/0/0/5 -  vlt_br/tag139 to r03
-#gi0/0/0/6 -  vlt_br/tag130 to r04
-
-if (sys.argv[1]) in ['r12']:
-    subprocess.call(['python', 'util/qemu-xrv9k.py', 'r12.img', '12', 'r12'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_mgt_br', 'rtr12mgt1'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr12xr0', 'tag=402'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr12xr5', 'tag=139'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr12xr6', 'tag=130'])
-
-# r03
-#gi0/0/0/0 -  vlt_br/tag127 to r11 
-#gi0/0/0/3 -  vlt_br/tag139 to r12
+# R03
+#gi0/0/0/0 -  jlpn_br/tag102 to R00 g0/0/0/2
+#gi0/0/0/1 -  jlpn_br/tag109 to R71 g1
+#gi0/0/0/2 -  jlpn_br/tag110 to R72 g1
+#gi0/0/0/3 -  jlpn_br/tag105 to R01 g0/0/0/1
+#gi0/0/0/4 -  jlpn_br/tag107 to R02 g0/0/0/1
 
 if (sys.argv[1]) in ['r03']:
     subprocess.call(['python', 'util/qemu-xrv9k.py', 'r03.img', '03', 'r03'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_mgt_br', 'rtr03mgt1'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr03xr0', 'tag=127'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr03xr3', 'tag=139'])
-
-# r4
-#gi0/0/0/0 -  vlt_br/tag404 to r07 
-#gi0/0/0/3 -  vlt_br/tag130 to r12
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_mgt_br', 'rtr04mgt1'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr03xr0', 'tag=102'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr03xr1', 'tag=109'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr03xr2', 'tag=110'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr03xr3', 'tag=105'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr03xr4', 'tag=107'])
+    
+# R04
+#gi0/0/0/0 -  jlpn_br/tag103 to R00 g0/0/0/2
+#gi0/0/0/1 -  jlpn_br/tag111 to R71 g1
+#gi0/0/0/2 -  jlpn_br/tag112 to R72 g1
+#gi0/0/0/3 -  jlpn_br/tag106 to R01 g0/0/0/1
+#gi0/0/0/4 -  jlpn_br/tag108 to R02 g0/0/0/1
 
 if (sys.argv[1]) in ['r04']:
     subprocess.call(['python', 'util/qemu-xrv9k.py', 'r04.img', '04', 'r04'])
     subprocess.call(['ovs-vsctl', 'add-port', 'vlt_mgt_br', 'rtr04mgt1'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr04xr0', 'tag=404'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr04xr3', 'tag=130'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr04xr0', 'tag=103'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr04xr1', 'tag=111'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr04xr2', 'tag=112'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr04xr3', 'tag=106'])
+    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr04xr4', 'tag=108'])
 
-# r7 - Dallas LSR
-#gi0/0/0/0 -  vlt_br/tag400 to r01 
-#gi0/0/0/1 -  vlt_br/tag401 to r05
-#gi0/0/0/2 -  vlt_br/tag404 to r04
-#gi0/0/0/7 -  vlt_br/tag405 to r01 
+# R71 External Peer
+#g1 - vlt_br/tag109 to r1 gi0/0/0/1 
+#g2 - vlt_br/tag110 to r2 gi0/0/0/1
+#g3 - not used
+#g4 - vlt_outside_br br to internet_user_vlan
 
-if (sys.argv[1]) in ['r07']:
-    subprocess.call(['python', 'util/qemu-xrv9k.py', 'r07.img', '07', 'r07'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_mgt_br', 'rtr07mgt1'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr07xr0', 'tag=400'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr07xr1', 'tag=401'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr07xr2', 'tag=404'])
-    subprocess.call(['ovs-vsctl', 'add-port', 'vlt_br', 'rtr07xr7', 'tag=405'])
+if (sys.argv[1]) in ['r71']:
+    subprocess.call(['virsh', 'start', 'r71'])
 
+# R72 External Peer
+#g1 - vlt_br/tag111 to r2 gi0/0/0/2 
+#g2 - vlt_br/tag112 to r1 gi0/0/0/2
+#g3 - not used
+#g4 - vlt_outside_br br to internet_user_vlan
+
+if (sys.argv[1]) in ['r72']:
+    subprocess.call(['virsh', 'start', 'r72'])
 
 ############################
 print "node started"
