0018229d4ae50da59ae64865e37ea0036b9ff741
brmcdoug <brmcdoug@cisco.com>
2021-03-05 10:33:23 -0800

    Delete restart_webrtcSvr.sh

--- util/restart_webrtcSvr.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-lxc-stop -n vltwebrtcsvr00
-ovs-vsctl del-port vlt_br vltwebrtc00
-lxc-start -n vltwebrtcsvr00
-ovs-vsctl del-port vlt_br vltwebrtc00
-ovs-vsctl add-port vlt_br vltwebrtc00 tag=100
-echo lxc-ls --active
-lxc-ls --active
